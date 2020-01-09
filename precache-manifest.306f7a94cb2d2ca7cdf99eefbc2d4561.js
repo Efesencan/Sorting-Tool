11f997f6dabcae9b87c6e52e43c137fd96e9dea1
Efesencan <efe.sencan@hotmail.com>
2020-01-10 02:29:46 +0300

    Updates

---
+++ precache-manifest.306f7a94cb2d2ca7cdf99eefbc2d4561.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05f57fffa8133d8f03317473a17c12bd",
+    "url": "/Efesencan/Sorting-Tool.git/index.html"
+  },
+  {
+    "revision": "6cffc4f79d9dc3a30f57",
+    "url": "/Efesencan/Sorting-Tool.git/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "d9cba1d013e99fe6ec31",
+    "url": "/Efesencan/Sorting-Tool.git/static/css/main.dd9fb0bb.chunk.css"
+  },
+  {
+    "revision": "6cffc4f79d9dc3a30f57",
+    "url": "/Efesencan/Sorting-Tool.git/static/js/2.42f1752b.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/Efesencan/Sorting-Tool.git/static/js/2.42f1752b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d9cba1d013e99fe6ec31",
+    "url": "/Efesencan/Sorting-Tool.git/static/js/main.1d05e364.chunk.js"
+  },
+  {
+    "revision": "5ec3b8534642737c3c6d",
+    "url": "/Efesencan/Sorting-Tool.git/static/js/runtime-main.d23bac1d.js"
+  }
+]);
